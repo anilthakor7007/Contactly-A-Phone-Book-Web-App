0c0621a8f103b15f4d5f548a8b4f1753ca9dc8d8
Anil <anilthakor7007@gmail.com>
2024-11-21 15:25:43 +0530

    prepare backend for deplyment

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    origin: '*', 
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
